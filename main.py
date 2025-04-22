c17a9d6ab11e0892406ce4cb2b4857471ccd5d2c
MarwanElfiky <127157681+MarwanElfiky@users.noreply.github.com>
2025-04-22 23:26:06 +0200

    Appropriatly read, filtered, plotted signals

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
@@ -12,23 +14,32 @@ root.geometry("400x400")
 
 def read_signal(signal):
     file_path = filedialog.askopenfilename(title=f"Choose Signal {signal}")
-    indices = []
+    rhythms = []
     amplitudes = []
     with open(file_path, 'r') as file:
         for line in file:
-            index, amplitude = line.split()
-            indices.append(int(index))
+            amplitude, rhythm = line.split()
             amplitudes.append(int(amplitude))
+            rhythms.append(int(rhythm))
     # print(f"First 20 Indices of {signal} Signal : {indices[:20]}")
     # print(f"First 20 Values {signal} Signal : {amplitudes[:20]}")
-    return indices, amplitudes
+    return amplitudes
 
 
-def plot_signal(signal_indices, signal_values):
-    plt.plot(signal_indices[:100], signal_values[:100])
-    plt.xlabel("Index")
-    plt.ylabel("Amplitudes")
+def plot_signal(x, y, subplots):
+    plt.figure(figsize=(10, 6))
+    for i in range(len(subplots)):
+        plt.subplot(subplots[i])
+        plt.plot(x[i][:400], y[i][:400])
+        plt.xlabel("Index")
+        plt.ylabel("Amplitudes")
+
     plt.show()
+
+
+def create_indices(signal_amplitudes):
+    signal_indices = np.arange(0, len(signal_amplitudes))
+    return list(signal_indices)
 
 
 def bandpass_filter(data, low_cutoff=1, high_cutoff=40, sampling_rate=250, order=2):
@@ -41,10 +52,15 @@ def bandpass_filter(data, low_cutoff=1, high_cutoff=40, sampling_rate=250, order
 
 
 def apply_bandpass(signal):
-    signal_indices, signal_values = read_signal(signal)
-    filtered_signal = bandpass_filter(signal_values)
-    plot_signal(signal_indices, filtered_signal)
-    return signal_indices, filtered_signal
+    signal_amplitudes = read_signal(signal)
+    signal_indices = create_indices(signal_amplitudes)
+    print(f"Indices : {signal_indices[:10]}\nN : {len(signal_indices)}")
+    print(f"Amplitudes : {signal_amplitudes[:10]}\nN : {len(signal_amplitudes)}")
+    filtered_signal = bandpass_filter(signal_amplitudes)
+    print(f"Filtered signal : {filtered_signal[:10]}\nN = : {len(filtered_signal)}")
+    plot_signal([signal_indices, signal_indices], [signal_amplitudes, filtered_signal], [121, 122])
+
+
 
 
 ali_signal_button = ttk.Button(root, text="Read Ali Signal", command=lambda: apply_bandpass("Ali"))
